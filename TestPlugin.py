e69fe54ef312de90bd72abc17bda49987ab99c05
Jake Taylor <me@jaketaylor.co>
2018-10-08 20:07:02 +0100

    Add return types to on/off

--- TestPlugin.py
+++ TestPlugin.py
@@ -6,13 +6,15 @@ class TestPlugin(FauxmoPlugin):
 
         super().__init__(name=name, port=port)
 
-    def on(self) -> None:
+    def on(self) -> bool:
         self.state = 'on'
         print('On!')
+        return true
 
-    def off(self) -> None:
+    def off(self) -> bool:
         self.state = 'off'
         print('Off!')
+        return true
 
     def get_state(self) -> str:
         return self.state
