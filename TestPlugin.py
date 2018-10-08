1870b56933f201fecac7766cc2f4b3d4147bb585
Jake Taylor <me@jaketaylor.co>
2018-10-08 20:32:26 +0100

    Add GPIO controls

--- TestPlugin.py
+++ TestPlugin.py
@@ -1,20 +1,71 @@
 from fauxmo.plugins import FauxmoPlugin
+import RPi.GPIO as gpio
 
 class TestPlugin(FauxmoPlugin):
-    def __init__(self, name: str, port: int) -> None:
+    """
+    Initiates the plugin.
+    """
+    def __init__(self, name: str, port: int, channel: int) -> None:
+        # set default state
         self.state = 'off'
 
+        # get the channel pin map
+        channelPinMap = self.get_channel_pin_dict()
+
+        # store the channel and pin
+        self.channel = channel
+        self.pin = channelPinMap[channel]
+
+        # initiate the GPIO pins
+        gpio.setmode(gpio.BCM)
+        gpio.setup(self.pin, gpio.OUT)
+
+        # initiate parent class
         super().__init__(name=name, port=port)
 
+    """
+    Turns on the relay channel.
+    """
     def on(self) -> bool:
+        # adjust state
         self.state = 'on'
-        print('On!')
-        return True
 
+        try:
+            # turn on the channel
+            gpio.output(self.pin, gpio.HIGH)
+
+            return True
+        except:
+            self.state = 'unknown'
+            return False
+
+    """
+    Turns off the relay channel.
+    """
     def off(self) -> bool:
+        # adjust state
         self.state = 'off'
-        print('Off!')
-        return True
 
+        try:
+            # turn off the channel
+            gpio.output(self.pin. GPIO.LOW)
+
+            return True
+        except:
+            self.state = 'unknown'
+            return False
+
+    """
+    Gets the current state.
+    """
     def get_state(self) -> str:
         return self.state
+
+    """
+    Returns a dictionary like:
+    relay_channel -> BCM Pin
+    """
+    def get_channel_pin_dict():
+        return {
+            8: 2
+        }
